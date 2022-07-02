62f1bb63934dcef308770d0c7f86e7aee3b2ddbf
Roman Resenchuk <r@resrom.com>
2022-07-02 16:05:20 +0800

    Initial Commit

---
+++ Home-assignment-SprintFWD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home-assignment-SprintFWD
+//
+//  Created by Roman Resenchuk on 2/7/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
